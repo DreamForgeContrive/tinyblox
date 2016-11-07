87b88987fa8b91f4aa60ed3d6ea170fdcd290083
Kiran Vemuri <kvemuri@adaranetworks.com>
2016-11-07 11:19:12 -0800

    changes to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(name='sentinelpy',
       packages = ['sentinelpy'],
-      version='0.1',
+      version='0.1.1',
       description='Python Automation Framework',
       author='Kiran Vemuri',
       author_email='kkvemuri@uh.edu',
       url='https://github.com/DreamForgeContrive/sentinelpy',
-      download_url='https://github.com/DreamForgeContrive/sentinelpy/tarball/0.1',
+      download_url='https://github.com/DreamForgeContrive/sentinelpy/tarball/0.1.1',
       keywords=['automation', 'ssh', 'sftp', 'openstack', 'logging', 'cassandra'],
       install_reqires=['cassandra-driver', 'paramiko==1.17.0', 'pyyaml', 'requests'],
       classifiers=[],)
