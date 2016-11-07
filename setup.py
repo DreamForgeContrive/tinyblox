f18c044e514c77639d17cd530956e052df2524a5
Kiran Vemuri <kvemuri@adaranetworks.com>
2016-11-07 12:01:16 -0800

    Adding v0.1.4 tag, license, setup.py and setup.cfg attempting to upload to pypi for pip install support

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 setup(name='sentinelpy',
       packages=['sentinelpy'],
-      version='0.1.3',
+      version='0.1.4',
       description='Python Automation Framework',
       author='Kiran Vemuri',
       author_email='kkvemuri@uh.edu',
       url='https://github.com/DreamForgeContrive/sentinelpy',
-      download_url='https://github.com/DreamForgeContrive/sentinelpy/tarball/0.1.3',
+      download_url='https://github.com/DreamForgeContrive/sentinelpy/tarball/0.1.4',
       keywords=['automation', 'ssh', 'sftp', 'openstack', 'logging', 'cassandra'],
+      license='MIT',
       install_reqires=[
           'cassandra-driver',
           'paramiko==1.17.0',
