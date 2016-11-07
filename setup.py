975e899ae5b566611d19c276fca4e00c535bfb35
Kiran Vemuri <kvemuri@adaranetworks.com>
2016-11-07 11:48:20 -0800

    using setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='sentinelpy',
-      packages = ['sentinelpy'],
+      packages=['sentinelpy'],
       version='0.1.1',
       description='Python Automation Framework',
       author='Kiran Vemuri',
@@ -9,5 +12,9 @@ setup(name='sentinelpy',
       url='https://github.com/DreamForgeContrive/sentinelpy',
       download_url='https://github.com/DreamForgeContrive/sentinelpy/tarball/0.1.1',
       keywords=['automation', 'ssh', 'sftp', 'openstack', 'logging', 'cassandra'],
-      install_reqires=['cassandra-driver', 'paramiko==1.17.0', 'pyyaml', 'requests'],
+      install_reqires=[
+          'cassandra-driver',
+          'paramiko==1.17.0',
+          'requests',
+          'pyyaml'],
       classifiers=[],)
